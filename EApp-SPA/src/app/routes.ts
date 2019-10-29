4daebe154a7a72efeb1a852ec6902fcec93a366e
Stefan Vallet <stefanvallet99@gmail.com>
2019-10-29 18:24:00 -0300

    routes, guards, eof7

---
+++ EApp-SPA/src/app/routes.ts
@@ -0,0 +1,25 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+const routes: Routes = [
+  {  },
+];
+
+export const appRoutes: Routes = [
+  {path: 'home', component : HomeComponent},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'members', component : MemberListComponent, canActivate: [AuthGuard]},
+      {path: 'messages', component : MessagesComponent},
+      {path: 'lists', component : ListsComponent},
+    ]
+  },
+  {path: '**', redirectTo : 'home', pathMatch: 'full'},
+];
