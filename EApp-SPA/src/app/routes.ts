684aaa7151fa8284c796cd56cf6f953077d057ab
Stefan Vallet <stefanvallet99@gmail.com>
2019-11-05 19:34:38 -0300

    eof9

--- EApp-SPA/src/app/routes.ts
+++ EApp-SPA/src/app/routes.ts
@@ -4,6 +4,9 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolver/member-detail.resolver';
+import { MemberListResolver } from './_resolver/member-list.resolver';
 
 const routes: Routes = [
   {  },
@@ -16,7 +19,8 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      {path: 'members', component : MemberListComponent, canActivate: [AuthGuard]},
+      {path: 'members', component : MemberListComponent, resolve: {users: MemberListResolver}},
+      {path: 'members/:id', component : MemberDetailComponent, resolve: {user: MemberDetailResolver}},
       {path: 'messages', component : MessagesComponent},
       {path: 'lists', component : ListsComponent},
     ]
